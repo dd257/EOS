d591a9522c6c43e1253d6a778a2ea0af7cba8849
dd259 <drahul.dd@gmail.com>
2021-12-13 09:20:21 +0530

    Shared Memory

---
+++ sharedmem/p2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int *count;
+
+int main(int argc, const char* argv[])
+{
+    int shm;
+
+    shm = shm_open("/shm",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+    ftruncate(shm,sizeof(int));
+    count = (int *)mmap(NULL,sizeof(int),PROT_READ|PROT_WRITE,MAP_SHARED,shm,0);
+
+    while(1)
+    {
+        printf("%d\n",*count);
+        sleep(1);
+    }    
+}
